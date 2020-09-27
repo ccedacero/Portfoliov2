69b49e87a0767b5f58cf233be5c4593d32bfe9c0
ccedacero <cristian.cedacero@gmail.com>
2020-09-26 22:10:46 -0400

    clean code and add bounce to short text

--- js/js/script.js
+++
@@ -1,5 +0,0 @@
-let bar = document.querySelector("#progress");
-window.addEventListener("scroll", () => {
-  let max = document.body.scrollHeight - innerHeight;
-  bar.style.width = `${(pageYOffset / max) * 100}%`;
-});
